c5610ea5540660eb7073ab0f57bc6c516456937e
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-09-01 11:18:21 +0500

    Lecture7 task 1 Собрать строку с числами от a до b, a ≤ b

---
+++ task07_1_StringAB/Program.cs
@@ -0,0 +1,24 @@
+﻿// Собрать строку с числами от a до b, a ≤ b
+
+string NumberFor(int a, int b)
+{
+  string result = String.Empty;
+  for (int i = a; i <= b; i++)
+  {
+    result += $"{i} ";
+  }
+  return result;
+}
+
+string NumbersRec(int a, int b)
+{
+  if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+  else return String.Empty;
+
+  // На одну итерацию меньше.
+  // if (a < b) return $"{a} " + NumbersRec(a + 1, b);
+  // else return $"{b}";
+}
+
+Console.WriteLine(NumberFor(1, 10));
+Console.WriteLine(NumbersRec(1, 10));
